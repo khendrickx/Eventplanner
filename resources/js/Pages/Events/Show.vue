b391adc148b8b1869894f7cfad4dabce75a439b7
Kilian Hendrickx <mail@kilianhendrickx.be>
2026-05-22 20:08:18 +0200

    feat: events/show stub (map editor placeholder)

--- resources/js/Pages/Events/Show.vue
+++ resources/js/Pages/Events/Show.vue
@@ -1,7 +1,20 @@
 <script setup>
-defineProps({ event: Object })
+import { Head } from '@inertiajs/vue3'
+
+defineProps({
+    event: Object,
+})
 </script>
 
 <template>
-    <div></div>
+    <Head :title="event.name" />
+    <div class="h-screen flex flex-col">
+        <div class="px-4 py-3 border-b flex items-center gap-4 bg-white">
+            <span class="font-semibold">{{ event.name }}</span>
+            <span class="text-xs text-gray-500 capitalize">{{ event.role }}</span>
+        </div>
+        <div class="flex-1 flex items-center justify-center text-gray-400">
+            Map editor coming in Plan 2
+        </div>
+    </div>
 </template>
